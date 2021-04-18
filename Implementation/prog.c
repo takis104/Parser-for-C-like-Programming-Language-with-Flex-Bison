936293c9324a2a95ab4cf15c37962591beb8a004
Christos Balatsouras <52033650+takis104@users.noreply.github.com>
2021-04-18 03:15:13 +0300

    Add files via upload

---
+++ Implementation/prog.c
@@ -0,0 +1,5 @@
+PROGRAM __my_Program
+FUNCTION f_Name(argc, arg[10])
+VARS CHAR _val1[10], val2, val3[25];
+VARS INTEGER _val1[10], val2, val3[25];
+var2 = var3 + 5 * 2^(3-7/(var1+var3)); var3 = f_Name(var1, var2);
